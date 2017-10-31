5c697c443f6b92115d1c283cd800ff436da1c0b9
Abdul Jafar <abdul.jafar@bookyshow.com>
2017-10-31 14:20:15 +0530

    CRUD operations complted

--- src/app.js
+++ src/app.js
@@ -5,16 +5,38 @@ import React from 'react';
 import { render } from 'react-dom';
 
 //3. define reducers
-const reducer = function(state = { books: [] }, action) {
+const reducer = function(state ={books:[]}, action) {
     switch (action.type) {
         case 'A':
-            //let books = state.books.concat(action.payload);
+            let books = state.books.concat(action.payload);            
             //spread operator > babel presets
             return { books: [...state.books, ...action.payload] };
             break;
-        case 'Delete':
+        case 'DELETE_BOOK':
             const currentBookToRemove = [...state.books];
-            //const intexToDelete = currentBookToRemove.
+            const indexToDelete = currentBookToRemove.findIndex(function(book){
+                return book.id === action.payload.id;
+            })
+            return {books:[...currentBookToRemove.slice(0,indexToDelete),...currentBookToRemove.slice(indexToDelete+1)]}
+            break;
+
+        case 'UPDATE_BOOK':
+            //create a copy of the current array of books
+            const currentBookToUpdate = [...state.books];
+            //Determine at which index in books array is to be updated
+            const indexToUpdate = currentBookToUpdate.findIndex(function(book){
+                return book.id === action.payload.id;
+            })
+            //create a new book object with the new values and with the same array index of the item we want to replace
+            //To archive this we will use ...spread but we could use concat method too
+
+            const newBookToUpdate={
+                ...currentBookToUpdate[indexToUpdate],
+                description:action.payload.description
+            }
+            console.log('what is it newBookToUpdate :',newBookToUpdate);
+            //use slice to remove the book at the specified index, replace with the nw object and concatenate with the rest of item in the array
+            return {books:[...currentBookToUpdate.slice(0,indexToUpdate),newBookToUpdate,...currentBookToUpdate.slice(indexToUpdate+1)]}
             break;
     }
     return state;
@@ -42,19 +64,19 @@ store.dispatch({
     }]
 })
 
+//DELETE a book
 store.dispatch({
-    type: 'A',
-    payload: [{
-        id: 3,
-        title: 'Book3',
-        description: 'This is book3',
-        price: 150
-    }]
+    type: 'DELETE_BOOK',
+    payload: {
+        id: 2
+    }
 })
 
+//UPDATE a book
 store.dispatch({
-    type: 'Delete',
-    payload: {
-        id: 3
+    type:'UPDATE_BOOK',
+    payload:{
+        id:1,
+        description:'This is updated version book'
     }
 })
