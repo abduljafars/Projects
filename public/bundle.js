fe04288467b0c57216e58e57d754a7735715ada4
Abdul Jafar <abdul.jafar@bookyshow.com>
2017-11-07 13:49:24 +0530

    Implemented: display dynamic badge on cart

--- public/bundle.js
+++ public/bundle.js
@@ -32484,11 +32484,11 @@ var Menu = function (_React$Component) {
               _reactBootstrap.NavItem,
               { eventKey: 2, href: '/cart' },
               'Your Cart',
-              _react2.default.createElement(
+              this.props.cartItemsNumber > 0 ? _react2.default.createElement(
                 _reactBootstrap.Badge,
                 { className: 'badge' },
-                '1'
-              )
+                this.props.cartItemsNumber
+              ) : ''
             )
           )
         )
@@ -45028,6 +45028,8 @@ var _footer = __webpack_require__(493);
 
 var _footer2 = _interopRequireDefault(_footer);
 
+var _reactRedux = __webpack_require__(56);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -45051,7 +45053,7 @@ var Main = function (_React$Component) {
 			return _react2.default.createElement(
 				'div',
 				null,
-				_react2.default.createElement(_menu2.default, null),
+				_react2.default.createElement(_menu2.default, { cartItemsNumber: this.props.totalQty }),
 				this.props.children,
 				_react2.default.createElement(_footer2.default, null)
 			);
@@ -45061,7 +45063,12 @@ var Main = function (_React$Component) {
 	return Main;
 }(_react2.default.Component);
 
-exports.default = Main;
+function mapStateToProps(state) {
+	return {
+		totalQty: state.cart.totalQty
+	};
+}
+exports.default = (0, _reactRedux.connect)(mapStateToProps)(Main);
 
 /***/ }),
 /* 521 */
