68957922959c464f91c424f9b7381426fd6db234
Abdul Jafar <abdul.jafar@bookyshow.com>
2017-11-07 11:12:04 +0530

    Implemented: Calculate totals in the cart

--- public/bundle.js
+++ public/bundle.js
@@ -32320,6 +32320,7 @@ Object.defineProperty(exports, "__esModule", {
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 exports.cartReducers = cartReducers;
+exports.totals = totals;
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
@@ -32329,7 +32330,11 @@ function cartReducers() {
 
 	switch (action.type) {
 		case "ADD_TO_CART":
-			return { cart: [].concat(_toConsumableArray(state), _toConsumableArray(action.payload)) };
+			return _extends({}, state, {
+				cart: action.payload,
+				totalAmount: totals(action.payload).amount,
+				totalQty: totals(action.payload).qty
+			});
 			break;
 		case "UPDATE_CART":
 			var currentBookToUpdate = [].concat(_toConsumableArray(state.cart));
@@ -32340,13 +32345,31 @@ function cartReducers() {
 				quantity: currentBookToUpdate[indexToUpdate].quantity + action.unit
 			});
 			var cartUpdate = [].concat(_toConsumableArray(currentBookToUpdate.slice(0, indexToUpdate)), [newBookToUpdate], _toConsumableArray(currentBookToUpdate.slice(indexToUpdate + 1)));
-			return _extends({}, state, { cart: cartUpdate });
+			return _extends({}, state, { cart: cartUpdate, totalAmount: totals(cartUpdate).amount, totalQty: totals(cartUpdate).qty });
 			break;
 		case "DELTE_CART_ITEM":
-			return { cart: [].concat(_toConsumableArray(state), _toConsumableArray(action.payload)) };
+			return _extends({}, state, { cart: action.payload, totalAmount: totals(action.payload).amount, totalQty: totals(action.payload).qty });
 			break;
 	}
 	return state;
+}
+
+// calculate totals
+
+function totals(payloadArr) {
+	var totalAmount = payloadArr.map(function (cartArr) {
+		return cartArr.price * cartArr.quantity;
+	}).reduce(function (a, b) {
+		return a + b;
+	}, 0); //start summing from index0
+
+	var totalQty = payloadArr.map(function (qty) {
+		return qty.quantity;
+	}).reduce(function (a, b) {
+		return a + b;
+	}, 0);
+
+	return { amount: totalAmount.toFixed(2), qty: totalQty };
 }
 
 /***/ }),
@@ -44160,7 +44183,8 @@ var Cart = function (_React$Component) {
 						_react2.default.createElement(
 							'h6',
 							null,
-							'Total Amount:'
+							'Total Amount:',
+							this.props.totalAmount
 						),
 						_react2.default.createElement(
 							_reactBootstrap.Button,
@@ -44204,7 +44228,8 @@ var Cart = function (_React$Component) {
 							_react2.default.createElement(
 								'h6',
 								null,
-								'Total $:'
+								'Total $:',
+								this.props.totalAmount
 							)
 						),
 						_react2.default.createElement(
@@ -44223,7 +44248,8 @@ var Cart = function (_React$Component) {
 
 function mapStateToProps(state) {
 	return {
-		cart: state.cart.cart
+		cart: state.cart.cart,
+		totalAmount: state.cart.totalAmount
 	};
 }
 
